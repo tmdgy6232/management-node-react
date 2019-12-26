988abe6d50f576b8b2de3e501fc3459d29e27baa
tmdgy6232 <tmdgy6232@naver.com>
2019-12-26 12:10:50 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Customer from "./conponents/Customer";
+import "./App.css";
+
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "임승효",
+    birthday: "961222",
+    gender: "남자",
+    job: "아이티꿈나무"
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "김으녜",
+    birthday: "981123",
+    gender: "여자",
+    job: "꿈나무"
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "문승연",
+    birthday: "000000",
+    gender: "여자",
+    job: "리얼꿈나무"
+  }
+];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src ={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
+    <div>
+      {customers.map(c => {
+        return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        );
+      })}
     </div>
   );
 }
